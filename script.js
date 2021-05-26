d8b3d2e5ee282f9515412d717184117f24a04607
“anisio2” <anisio2@gmail.com>
2021-05-26 15:32:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+let name = "Anisio"
+let idade = 30
+let ano = 2021
+let anoNascimento =  (ano - idade)
+console.log("Meu ano de nascimento é ", anoNascimento)
+
+let souhumano = "true"
+console.log("Eu sou humano", souhumano)
+
+let number = 318
+let rebmun = 813
+console.log(number + rebmun)
